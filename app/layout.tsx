6aa0ca804af41c5701bbfa96ecebb85a3c835f40
Shahm Najeeb <Nirt_12023@outlook.com>
2025-10-24 18:54:21 +0400

    Add favicon link to layout for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
         <head>
-            <title>WESMUN 2026</title></head>
+            <title>WESMUN 2026</title>
+            <link rel="icon" href="/wesmun.svg" />
+        </head>
         <body className="font-sans antialiased">
         {children}
         <Analytics />

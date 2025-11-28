8cf76c70d97c7ee7b6c6288b157d176b253e4542
Shahm Najeeb <Nirt_12023@outlook.com>
2025-11-28 22:04:10 +0400

    Add sitemap and robots.txt configuration for SEO optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+    siteUrl: "https://wesmun.com",
+    generateRobotsTxt: true,
+    changefreq: "daily",
+    priority: 0.7,
+    sitemapSize: 5000,
+};
